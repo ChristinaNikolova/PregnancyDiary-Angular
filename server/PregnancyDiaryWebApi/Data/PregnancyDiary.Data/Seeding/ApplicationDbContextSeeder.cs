e302ab95f76b8e88907bbb211d2d34959e532123
christinanikolova <christina.nikolova@abv.bg>
2021-05-06 08:22:10 +0300

    add seeders

---
+++ server/PregnancyDiaryWebApi/Data/PregnancyDiary.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -0,0 +1,39 @@
+﻿namespace PregnancyDiary.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using PregnancyDiary.Data.Seeding.CustomSeeder;
+
+    public class ApplicationDbContextSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException(nameof(dbContext));
+            }
+
+            if (serviceProvider == null)
+            {
+                throw new ArgumentNullException(nameof(serviceProvider));
+            }
+
+            var seeders = new List<ISeeder>
+                          {
+                              new RolesSeeder(),
+                              new UsersSeeder(),
+                              new UsersToRolesSeeder(),
+                              new CategoriesSeeder(),
+                              new ArticlesSeeder(),
+                          };
+
+            foreach (var seeder in seeders)
+            {
+                await seeder.SeedAsync(dbContext, serviceProvider);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
