662a5c4645df481868faeee8ed731436347ac374
christinanikolova <christina.nikolova@abv.bg>
2021-05-06 07:26:53 +0300

    add automapper and automapper cofigs

---
+++ server/PregnancyDiaryWebApi/Web/PregnancyDiary.Web.Models/Common/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PregnancyDiary.Web.Models.Common.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId
+            => !string.IsNullOrEmpty(this.RequestId);
+    }
+}
