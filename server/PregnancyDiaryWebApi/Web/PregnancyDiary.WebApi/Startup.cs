970d03ff86c4ed247ea363bcff4b0f198fd9df4f
christinanikolova <christina.nikolova@abv.bg>
2021-05-06 09:00:01 +0300

    create Db, seed Data

--- server/PregnancyDiaryWebApi/Web/PregnancyDiary.WebApi/Startup.cs
+++ server/PregnancyDiaryWebApi/Web/PregnancyDiary.WebApi/Startup.cs
@@ -1,15 +1,27 @@
 ﻿namespace PregnancyDiary.WebApi
 {
     using System.Reflection;
+    using System.Text;
 
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.IdentityModel.Tokens;
     using Microsoft.OpenApi.Models;
+    using PregnancyDiary.Data;
+    using PregnancyDiary.Data.Common.Repositories;
+    using PregnancyDiary.Data.Models;
+    using PregnancyDiary.Data.Repositories;
+    using PregnancyDiary.Data.Seeding;
     using PregnancyDiary.Services.Mapping;
     using PregnancyDiary.Web.Models.Common.ViewModels;
+    using PregnancyDiary.WebApi.Helpers;
 
     public class Startup
     {
@@ -23,7 +35,73 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(
+                   this.Configuration.GetConnectionString("DefaultConnection")));
+
+            var jwtSettingsSection = this.Configuration.GetSection("JwtSettings");
+            services.Configure<JwtSettings>(jwtSettingsSection);
+
+            var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+
+            var key = Encoding.ASCII.GetBytes(jwtSettings.Secret);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddControllers();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services
+                .AddAuthentication()
+                .AddIdentityServerJwt();
+
+            // Data repositories
+            services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+
+            //services.AddTransient<IArticlesService, ArticlesService>();
+            //services.AddTransient<ICategoriesService, CategoriesService>();
+            //services.AddTransient<IUserArticleLikesService, UserArticleLikesService>();
+            //services.AddTransient<ICommentsService, CommentsService>();
+            //services.AddTransient<IUsersService, UsersService>();
+            //services.AddTransient<IDiariesService, DiariesService>();
+            //services.AddTransient<IWeeksService, WeeksService>();
+            //services.AddTransient<IMemoriesService, MemoriesService>();
+            //services.AddTransient<IBabiesService, BabiesService>();
+
+            services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PregnancyDiary.WebApi", Version = "v1" });
@@ -35,6 +113,25 @@
         {
             AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
 
+            app.UseCors(x => x
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials()
+               .WithOrigins("http://localhost:4200"));
+
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider
+                    .GetRequiredService<ApplicationDbContext>();
+
+                dbContext.Database.Migrate();
+
+                new ApplicationDbContextSeeder()
+                    .SeedAsync(dbContext, serviceScope.ServiceProvider)
+                    .GetAwaiter()
+                    .GetResult();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -43,14 +140,16 @@
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
             });
         }
     }
