7efac6bb061fbe5cc43954403bd9dde7d3450264
christinanikolova <christina.nikolova@abv.bg>
2021-06-05 19:28:32 +0300

    create authService, start working on home page

---
+++ client/src/app/core/services/auth/auth-service.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import IRegister from 'src/app/components/shared/models/authentication/IRegister';
+import ILogin from 'src/app/components/shared/models/authentication/ILogin';
+
+const loginUrl = environment.apiBaseUrl + 'account/login';
+const registerUrl = environment.apiBaseUrl + 'account/register';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthServiceService {
+  private readonly isUserAdmin: boolean;
+
+  constructor(private http: HttpClient) {
+    this.isUserAdmin = false;
+  }
+
+  register(body: IRegister) {
+    return this.http.post(registerUrl, body);
+  }
+
+  login(body: ILogin) {
+    return this.http.post(loginUrl, body);
+  }
+
+  logout() {
+    return localStorage.clear();
+  }
+
+  isAuthenticated() {
+    return localStorage.getItem('token') !== null;
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  isAdmin(): boolean {
+    return localStorage.getItem('isAdmin') === 'true'
+      ? !this.isUserAdmin
+      : this.isUserAdmin;
+  }
+}
