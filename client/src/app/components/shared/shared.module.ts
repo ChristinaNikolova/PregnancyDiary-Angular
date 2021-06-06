f894668b36d0b49b548f569aeee19db581a0398f
christinanikolova <christina.nikolova@abv.bg>
2021-06-06 10:18:49 +0300

    fix some UI bugs

--- client/src/app/components/shared/shared.module.ts
+++ client/src/app/components/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 import { LoginRegisterPictureComponent } from './login-register-picture/login-register-picture.component';
 
 @NgModule({
   declarations: [
-  LoginRegisterPictureComponent],
+  LoginRegisterPictureComponent
+],
   imports: [
     CommonModule,
     RouterModule,
