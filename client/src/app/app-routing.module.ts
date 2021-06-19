6db805006c94518c6490e399ec11c21114fd4ad0
christinanikolova <christina.nikolova@abv.bg>
2021-06-19 09:53:35 +0300

    add NotFound page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/authentication/login/login.component';
 import { RegisterComponent } from './components/authentication/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'articles', loadChildren: './components/article/article.module#ArticleModule'}
+  { path: 'articles', loadChildren: './components/article/article.module#ArticleModule' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- client/src/app/components/shared/shared.module.ts
+++ client/src/app/components/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { LoginRegisterPictureComponent } from './login-register-picture/login-register-picture.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-  LoginRegisterPictureComponent
+  LoginRegisterPictureComponent,
+  NotFoundComponent
 ],
   imports: [
     CommonModule,
