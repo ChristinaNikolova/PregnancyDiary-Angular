923c218caca77b46ab26902a1fd2d8ba82be16ff
christinanikolova <christina.nikolova@abv.bg>
2021-07-10 16:21:10 +0300

    add some unit tests to test Karma and Jasmine

--- client/src/app/core/services/articles/articles.service.spec.ts
+++ client/src/app/core/services/articles/articles.service.spec.ts
@@ -1,16 +1,92 @@
-import { TestBed } from '@angular/core/testing';
+import { defer } from 'rxjs';
 
+import IArticle from 'src/app/components/shared/models/article/IArticle';
 import { ArticlesService } from './articles.service';
 
+ function asyncData<T>(data: T) {
+  return defer(() => Promise.resolve(data));
+}
+
 describe('ArticlesService', () => {
+  let httpClientSpy: {get: jasmine.Spy, post: jasmine.Spy}
   let service: ArticlesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ArticlesService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+    service=new ArticlesService(<any> httpClientSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return all articles', (done) => {
+    let expectedArticles: IArticle[] = [
+      {
+        id: '1',
+        title: 'title 1',
+        picture: '',
+        categoryId: '1',
+        categoryName: 'test category',
+        likesCount: 1,
+        commentsCount: 1,
+        content: 'test content',
+        shortContent: 'test content',
+        createdOnAsString: '10.07.2021',
+      },
+      {
+        id: '2',
+        title: 'title 2',
+        picture: '',
+        categoryId: '1',
+        categoryName: 'test category',
+        likesCount: 2,
+        commentsCount: 2,
+        content: 'test content',
+        shortContent: 'test content',
+        createdOnAsString: '09.07.2021',
+      },
+    ];
+
+    httpClientSpy.get.and.returnValue(asyncData(expectedArticles));
+    service.all().subscribe((data) =>{
+      expect(data).toEqual(expectedArticles)
+    });
+    done();
+  });
+
+  it('should return orderedarticles', (done) => {
+    let expectedArticles: IArticle[] = [
+      {
+        id: '2',
+        title: 'title 2',
+        picture: '',
+        categoryId: '1',
+        categoryName: 'test category',
+        likesCount: 2,
+        commentsCount: 2,
+        content: 'test content',
+        shortContent: 'test content',
+        createdOnAsString: '09.07.2021',
+      },
+      {
+        id: '1',
+        title: 'title 1',
+        picture: '',
+        categoryId: '1',
+        categoryName: 'test category',
+        likesCount: 1,
+        commentsCount: 1,
+        content: 'test content',
+        shortContent: 'test content',
+        createdOnAsString: '10.07.2021',
+      },
+    ];
+
+    httpClientSpy.get.and.returnValue(asyncData(expectedArticles));
+    service.order('likes').subscribe((data) =>{
+      expect(data).toEqual(expectedArticles)
+    });
+    done();
   });
 });
